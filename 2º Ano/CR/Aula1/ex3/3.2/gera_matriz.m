9151fea9ab465b316e70f77d113c5d30d8aacf76
Rúben Santos <lcs_ruben@hotmail.com>
2023-02-22 14:58:20 +0000

    updt

--- 2º Ano/CR/Aula1/ex3/3.2/gera_matriz.m
+++
@@ -1,9 +0,0 @@
-function mat = gera_matriz(a,b,x)
-% Inputs:
-% a - número de linhas da matriz a gerar 
-% b - número de colunas da matriz a gerar 
-% x - limite maximo dos numeros aleatorios
-% Output:
-% m - matriz a * b inicializada com numeros inteiros
-mat = randi(x,a,b)
-end
